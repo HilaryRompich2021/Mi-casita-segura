5e8e87b46346a431249fbb6cb0c05527bff87386
Hilary Rompich <hrompichp1@miumg.edu.gt>
2025-04-22 23:22:31 -0600

    Datos de Reserva

--- Backend/Mi casita segura - backend/Mi-casita-segura/src/main/java/com/example/Mi/casita/segura/reservas/model/Reserva.java
+++ Backend/Mi casita segura - backend/Mi-casita-segura/src/main/java/com/example/Mi/casita/segura/reservas/model/Reserva.java
@@ -1,4 +1,39 @@
 package com.example.Mi.casita.segura.reservas.model;
 
+import com.example.Mi.casita.segura.usuarios.model.Usuario;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Data
 public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Área común reservada: salón, cancha, piscina, etc.
+    @Column(nullable = false)
+    private String areaComun;
+
+    @Column(nullable = false)
+    private LocalDate fecha;
+
+    @Column(nullable = false)
+    private LocalTime horaInicio;
+
+    @Column(nullable = false)
+    private LocalTime horaFin;
+
+    // Usuario que reservó (residente)
+    @ManyToOne
+    @JoinColumn(name = "asociado_a", referencedColumnName = "cui")
+    private Usuario residente;
+
+    // Este campo se agregará si quieres vincular reserva con detallePago
+    // @OneToOne(mappedBy = "reserva")
+    // private DetallePago detallePago
 }
