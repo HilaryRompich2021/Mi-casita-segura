7c9da28c57bf0dd2f0879fb7c7fc0318c5930ec2
Hilary Rompich <hrompichp1@miumg.edu.gt>
2025-05-09 01:25:53 -0600

    front vista no funcional registro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/ui/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  /*template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+  `,*/
 })
 export class AppComponent {
   title = 'mi-casita-tailwind';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter }        from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideZoneChangeDetection } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
+
+/*const routes: Routes = [
+  { path: '', redirectTo: 'registro', pathMatch: 'full' },
+  { path: 'registro', component: RegistroComponent },
+];*/
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideZoneChangeDetection({ eventCoalescing: true })
+  ]
+})
+.catch(err => console.error(err));
+/*bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).catch(err => console.error(err));
+*/
+
+/*bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+*/
